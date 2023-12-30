7a861089f82bc738c179fa969990a14a1f64431f
Razdor13 <oneamaday@gmail.com>
2023-12-30 11:40:21 +0200

    done 80%%

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
-
 const containerImg = document.querySelector(".containerimg");
-let nowPositionNoPx = parseFloat(containerImg.style.right);
-
+let isAnimating = false;
 prevBtn.addEventListener("click", () => {
-    console.log(nowPositionNoPx);
     let i = 100;
     function animationPrev() {
         if (i === 0) {
             return;
+        } else if (parseFloat(containerImg.style.right) === -1000) {
+            prevBtn.style.visibility = "hidden";
+        } else {
+            nextBtn.style.visibility = "visible";
         }
-        nowPositionNoPx -= 10;
         i -= 1;
-        containerImg.style.right = `${nowPositionNoPx}px`;
+        containerImg.style.right =
+            parseFloat(containerImg.style.right) - 10 + "px";
 
+        animation();
+        console.log(i);
+    }
+    function animation() {
+        setTimeout(animationPrev, 1);
+    }
+    animationPrev();
+});
+nextBtn.addEventListener("click", () => {
+    let i = 100;
+    function animationPrev() {
+        if (i === 0) {
+            return;
+        } else if (parseFloat(containerImg.style.right) === -1000) {
+            nextBtn.style.visibility = "hidden";
+        } else {
+            prevBtn.style.visibility = "visible";
+        }
+        i -= 1;
+        containerImg.style.right =
+            parseFloat(containerImg.style.right) + 10 + "px";
         animation();
         console.log(i);
     }
