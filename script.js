981412ca5657e1fefb60aa4111695e9b2970ed51
Razdor13 <oneamaday@gmail.com>
2024-01-03 10:41:21 +0200

    rework

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 const containerImg = document.querySelector(".containerimg");
-let isAnimating = false;
+let currentImgIndex = 1;
+const IMG_COUNT = 3;
+const IMG_WIDTH = 1000;
 prevBtn.addEventListener("click", () => {
-    let i = 100;
-    function animationPrev() {
-        if (i === 0) {
-            return;
-        } else if (parseFloat(containerImg.style.right) === -1000) {
-            prevBtn.style.visibility = "hidden";
-        } else {
-            nextBtn.style.visibility = "visible";
-        }
-        i -= 1;
-        containerImg.style.right =
-            parseFloat(containerImg.style.right) - 10 + "px";
+    currentImgIndex--;
+    updateImg();
+});
 
-        animation();
-        console.log(i);
-    }
-    function animation() {
-        setTimeout(animationPrev, 1);
-    }
-    animationPrev();
-});
 nextBtn.addEventListener("click", () => {
-    let i = 100;
-    function animationPrev() {
-        if (i === 0) {
-            return;
-        } else if (parseFloat(containerImg.style.right) === -1000) {
-            nextBtn.style.visibility = "hidden";
-        } else {
-            prevBtn.style.visibility = "visible";
-        }
-        i -= 1;
-        containerImg.style.right =
-            parseFloat(containerImg.style.right) + 10 + "px";
-        animation();
-        console.log(i);
-    }
-    function animation() {
-        setTimeout(animationPrev, 1);
-    }
-    animationPrev();
+    currentImgIndex++;
+    updateImg();
 });
+
+function updateImg() {
+    prevBtn.style.visibility = !currentImgIndex ? "hidden" : "visible";
+    nextBtn.style.visibility =
+        currentImgIndex === IMG_COUNT - 1 ? "hidden" : "visible";
+    const newX = currentImgIndex * IMG_WIDTH - IMG_WIDTH;
+    containerImg.style.transform = `translateX(${-newX}px)`;
+}
